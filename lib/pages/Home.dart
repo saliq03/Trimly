a38ad8eff51bf39ca2ac37c2a8c3e9488cd7b5df
saliq03 <javidsaliq@gmail.com>
2024-07-12 11:48:25 +0530

    Home screen designed

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -20,8 +20,8 @@ class _HomeState extends State<Home> {
               children: [
                 Column(crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Hello,",style: TextStyle(fontSize: 30,),),
-                    Text("Saliq Javid",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),)
+                    Text("Hello,",style: TextStyle(fontSize: 25,),),
+                    Text("Saliq Javid",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),)
                   ]),
 
                 Image.asset("assets/images/bg.png",height: 70,width: 70,fit: BoxFit.cover,)
@@ -29,15 +29,59 @@ class _HomeState extends State<Home> {
           Divider(color: Colors.black,),
           Container(
             padding: EdgeInsets.symmetric(horizontal: 25,vertical: 10),
-            child: Column(
+            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Services",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),)
+                Text("Services",style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold),),
+                SizedBox(height: 10,),
+                Row(
+                  children: [
+                    MyServices("assets/icons/shaving.png", "Classic Shaving"),
+                    SizedBox(width: 10,),
+                    MyServices("assets/icons/HairWashing.png", "Hair Washing")
 
+                  ],
+                ),
+                SizedBox(height: 10,),
+                Row(
+                  children: [
+                    MyServices("assets/icons/HairCutting.png", "Hair Cutting"),
+                    SizedBox(width: 10,),
+                    MyServices("assets/icons/trimming.png", "Beard Trimming")
+                  ],),
+                SizedBox(height: 10,),
+                Row(
+                  children: [
+                    MyServices("assets/icons/facial.png", "Facials"),
+                    SizedBox(width: 10,),
+                    MyServices("assets/icons/kids.png", "Kids Hair Cutting")
+
+                  ],
+                )
               ]
 
             ),
           )
         ],
+      ),
+    );
+  }
+
+  MyServices(String image,String title){
+    return Flexible(
+      fit:FlexFit.tight,
+      child: Container(
+        height: 170,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(22),
+            color: Color(0xFF3E2723)
+        ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(image,width: 80,height: 80,),
+            SizedBox(height: 20,),
+            Text(title,style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),)
+          ],
+        ),
       ),
     );
   }
